3f71fd63863387c1f801b87f6ba2a8108f5cfc78
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2025-01-06 10:03:28 +0500

    base url changed

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: 'http://127.0.0.1:8000'
+      apiBase: 'http://83.69.139.250:8001/'
     },
   },
   app: {
